f115afed393543a90a9e9a4b0a7ad2b286a917be
PragyaBharati <pragyabharati31@gmail.com>
2021-03-23 11:15:34 +0530

    Adding All Files

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nUnitTest
+{
+    class Program
+    {
+        public static List<Player> Player()
+        {
+            List<Player> Playerlist = new List<Player>
+            {
+            new Player{ PId=1,PName = "Sam",PTeam = "India"},
+             new Player{ PId=2,PName = "Sameer",PTeam = "India"},
+              new Player{ PId=3,PName = "Rohit",PTeam = "India"}
+
+            };
+            return Playerlist;
+            }
+
+    static void Main(string[] args)
+        {
+        }
+          
+    }
+}
